db38d1d09854ada3239ff95d3ab06f006b5d9c02
stranger <alirezaz313313@gmail.com>
2024-07-03 14:41:12 +0330

    .
    requirements

---
+++ prediction.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
